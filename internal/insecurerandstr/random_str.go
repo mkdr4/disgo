0fae1b7c5bc1f33ac5b3df909197f989fbfd64b5
mkdr4 <kirillmogilat@gmail.com>
2023-05-28 12:34:26 +0500

    push

---
+++ internal/insecurerandstr/random_str.go
@@ -0,0 +1,22 @@
+// Package insecurerandstr is not secure
+package insecurerandstr
+
+import (
+	"math/rand"
+	"time"
+)
+
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandStr returns a random string of the given length.
+func RandStr(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
